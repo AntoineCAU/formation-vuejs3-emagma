45f4b3b3f571c7c37f33123b46d4db4065a976df
Marion Agé <marion@les-tilleuls.coop>
2021-11-30 23:07:46 +0100

    add pwa

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
+  <ReloadPWA />
   <Navbar />
   <router-view></router-view>
 </template>
 
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import ReloadPWA from '@/components/ReloadPWA.vue';
 </script>
 
 <style lang="scss">
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+import { useRegisterSW } from 'virtual:pwa-register/vue'
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
@@ -8,6 +10,16 @@ import { router } from '@/router';
 import { store } from '@/store';
 
 library.add(fas);
+
+const intervalMS = 60 * 60 * 1000; // each hour
+
+const updateServiceWorker = useRegisterSW({
+  onRegistered(r: ServiceWorkerRegistration|undefined) {
+    r && setInterval(() => {
+      r.update()
+    }, intervalMS)
+  }
+});
 
 createApp(App)
   .use(router)
